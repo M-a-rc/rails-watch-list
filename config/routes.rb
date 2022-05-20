f2a4caaaf643d1f0315c198ef593afb5356d2eae
Marc Delesalle <mrc.delesalle@gmail.com>
2022-05-20 12:11:32 +0200

    .env

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :lists, only: %i[index create new show]
   resources :bookmarks, only: %i[new create destroy]
 end
